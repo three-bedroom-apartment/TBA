a772067ec26e2b5d705dcb8ded653fc107622ea7
three-bedroom-apartment <144286849+three-bedroom-apartment@users.noreply.github.com>
2023-10-25 17:02:26 +0200

    favicon update

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
   
   eleventyConfig.addPassthroughCopy("src/assets/css/style.css");
     eleventyConfig.addPassthroughCopy("src/assets/images");
+    eleventyConfig.addPassthroughCopy({ "src/assets/favicon": "/" });
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
     eleventyConfig.addFilter("postDate", (dateObj) => {
       return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
