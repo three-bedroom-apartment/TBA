28ba6e0a29b82a1dc6b5df8da84095a5075201b5
three-bedroom-apartment <144286849+three-bedroom-apartment@users.noreply.github.com>
2023-10-29 14:12:53 +0100

    Clean Up of old code

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,3 @@
-const { DateTime } = require("luxon");
 const fs = require("fs");
 
 module.exports = function(eleventyConfig) {
@@ -7,17 +6,12 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/images");
   eleventyConfig.addPassthroughCopy("src/assets/UI");
   eleventyConfig.addPassthroughCopy({ "src/assets/favicon": "/" });
-  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
-  eleventyConfig.addFilter("postDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-  });
   eleventyConfig.addShortcode("photoFolder", function (photoFolder) {
     const html = fs.readdirSync(photoFolder)
       .map((file) => `<img src="/assets/images/gallery/${file}" />`);
     return html.join("\n");
   });
   
-
   return {
     dir: {
       input: "src",
@@ -26,4 +20,5 @@ module.exports = function(eleventyConfig) {
       layouts: "_layouts",
     },
    };
+
 };
