c4391438f64cb9a80f98b340cb62cd3118e96ed0
three-bedroom-apartment <144286849+three-bedroom-apartment@users.noreply.github.com>
2023-10-29 14:05:10 +0100

    Release

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,29 @@
 const { DateTime } = require("luxon");
+const fs = require("fs");
 
 module.exports = function(eleventyConfig) {
   
   eleventyConfig.addPassthroughCopy("src/assets/css");
-    eleventyConfig.addPassthroughCopy("src/assets/images");
-    eleventyConfig.addPassthroughCopy("src/assets/UI");
-    eleventyConfig.addPassthroughCopy({ "src/assets/favicon": "/" });
-    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
-    eleventyConfig.addFilter("postDate", (dateObj) => {
-      return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-    });
+  eleventyConfig.addPassthroughCopy("src/assets/images");
+  eleventyConfig.addPassthroughCopy("src/assets/UI");
+  eleventyConfig.addPassthroughCopy({ "src/assets/favicon": "/" });
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+  eleventyConfig.addShortcode("photoFolder", function (photoFolder) {
+    const html = fs.readdirSync(photoFolder)
+      .map((file) => `<img src="/assets/images/gallery/${file}" />`);
+    return html.join("\n");
+  });
+  
 
-    return {
-      dir: {
-        input: "src",
-        data: "_data",
-        includes: "_includes",
-        layouts: "_layouts",
-      },
-    };
-  };
+  return {
+    dir: {
+      input: "src",
+      data: "_data",
+      includes: "_includes",
+      layouts: "_layouts",
+    },
+   };
+};
